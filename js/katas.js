ad159290a9bc6b001e54ab56e3d1d5be7f877639
Yaroslav Klimas <yarklim@gmail.com>
2022-11-04 17:53:06 +0200

    Update katas.js

--- js/katas.js
+++ js/katas.js
@@ -250,7 +250,9 @@ highAndLow("1 9 3 4 -5"); // return "9 -5"
 Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 */
-String.prototype.toJadenCase = function () {};
+String.prototype.toJadenCase = function () {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+};
 
 /*
 ? Вы уже реализовали класс Cube , но теперь нам снова нужна ваша помощь! 
