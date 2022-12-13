841eee7e7ad21097f3a51369b32f368695641ec3
Yaroslav Klimas <yarklim@gmail.com>
2022-12-13 21:45:40 +0200

    methods

--- js/katas-2.js
+++ js/katas-2.js
@@ -269,23 +269,3 @@ wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
 // console.log(solution('camelCasingTest')); // 'camel Casing Test'
 
 //? --------------------------------------------
-/*
-Ваша задача — написать функцию, которая принимает строку и 
-возвращает новую строку, в которой удалены все гласные.
-
-Например, строка «This website is for losers LOL!» станет 
-«Ths wbst s fr lsrs LL!».
-
-Примечание: для этой ката yне считается гласной.
-*/
-// function disemvowel(str) {
-//   return str;
-// }
-
-// console.log(disemvowel('This website is for losers LOL!')); // "Ths wbst s fr lsrs LL!"
-// console.log(
-//   disemvowel("No offense but,\nYour writing is among the worst I've ever read")
-// ); // "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd"
-// console.log(disemvowel('What are you, a communist?')); // "Wht r y,  cmmnst?"
-
-//? ----------------------------------------------
--- js/katas.js
+++ js/katas.js
@@ -1260,3 +1260,34 @@ var isSquare = function (n) {
 // console.log(isSquare(26)); // false
 
 //? ----------------------------------------
+/*
+Ваша задача — написать функцию, которая принимает строку и 
+возвращает новую строку, в которой удалены все гласные.
+
+Например, строка «This website is for losers LOL!» станет 
+«Ths wbst s fr lsrs LL!».
+
+Примечание: для этой ката y не считается гласной.
+*/
+function disemvowel(str) {
+  //   const vowelsArr = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  //   const lettersArr = str.split('');
+  //   const newStr = [];
+
+  //   console.log(lettersArr);
+
+  //   for (let i = 0; i < lettersArr.length; i++) {
+  //     if (vowelsArr.includes(lettersArr[i])) {
+  //       continue;
+  //     }
+  //     newStr.push(lettersArr[i]);
+  //   }
+  //   return newStr.join('');
+  return str.replace(/[aeiou]/gi, '');
+}
+
+// console.log(disemvowel('This website is for losers LOL!')); // "Ths wbst s fr lsrs LL!"
+// console.log(disemvowel("No offense but,\nYour writing is among the worst I've ever read")); // "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd"
+// console.log(disemvowel('What are you, a communist?')); // "Wht r y,  cmmnst?"
+
+//? ----------------------------------------------
