24700113ad4a2c27ff94f5d72884f3896d652080
Yaroslav Klimas <yarklim@gmail.com>
2022-12-30 19:01:45 +0200

    events

--- Methods/String-Methods.md
+++ Methods/String-Methods.md
@@ -1,3 +1,25 @@
+"Hello".charAt(4) 				=> o
+"Hello".concat("", "world") 	=> Hello world
+"Hello".startsWith("H") 		=> true
+"Hello".endsWith("o") 			=> o
+"Hello".includes("x)			=> false
+"Hello".indexOf("l")			=> 2
+"Hello".lastIndexOf("l")		=> 3
+"Hello".match(/[A-Z]/g)			=> ['H']
+"Hello".padStart(6, "?")		=> ?Hello
+"Hello".padEnd(6, "?")			=> Hello?
+"Hello".repeat(3)				=> HelloHelloHello
+"Hello".replace("llo", "y")		=> Hey
+"Hello".search("e")				=> 1
+"Hello".slice(1, 3)				=> el
+"Hello".split("")				=> ['H', 'e', 'l', 'l', 'o']
+"Hello".substring(2, 4)			=> ll
+"Hello".toLowerCase()			=> hello
+"Hello".toUpperCase()			=> HELLO
+" Hello ".trim()				=> Hello
+" Hello ".trimStart()			=> "Hello "
+" Hello ".trimEnd()				=> " Hello"
+
 ==========================================================================
 ========================= String.prototype.repeat() ======================
 ==========================================================================
--- js/module_6/events-pract.js
+++ js/module_6/events-pract.js
@@ -1,11 +1,80 @@
+/*
+* - Именование колбеков для слушателей:
+	- handle*: handleSubjectEvent
+	- *Handler: subjectEventHandler
+	- on*: onSubjectEvent
+*/
+
+// События кнопки
 const targetBtn = document.querySelector('.js-target-btn');
 const addListenerBtn = document.querySelector('.js-add-listener');
 const removeListenerBtn = document.querySelector('.js-remove-listener');
 
-targetBtn.addEventListener('click', () => {
-  console.log('Click');
+function onTargetButtonClick() {
+  console.log('Click on target Button');
+}
+
+addListenerBtn.addEventListener('click', () => {
+  console.log('Click on Add Button');
+
+  targetBtn.addEventListener('click', onTargetButtonClick);
 });
 
-function logMessage() {
-  console.log('Click for Button');
+removeListenerBtn.addEventListener('click', () => {
+  console.log('Remove listener');
+
+  targetBtn.removeEventListener('click', onTargetButtonClick);
+});
+
+// Событие сабмита форм
+const formEl = document.querySelector('.js-register-form');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+
+  formData.forEach((value, name) => {
+    console.log('name: ', name);
+    console.log('value: ', value);
+  });
+}
+
+// const input = document.querySelector('.js-input');
+// const nameLabel = document.querySelector('.js-button > span');
+// const licenseCheckbox = document.querySelector('.js-license');
+// const btn = document.querySelector('.js-button');
+
+// Заменяю одним объектом refs все вышенаписанные переменные:
+
+const refs = {
+  input: document.querySelector('.js-input'),
+  nameLabel: document.querySelector('.js-button > span'),
+  licenseCheckbox: document.querySelector('.js-license'),
+  btn: document.querySelector('.js-button'),
+};
+
+// refs.input.addEventListener('focus', onInputFocus);
+// refs.input.addEventListener('blur', onInputBlur);
+// refs.input.addEventListener('change', onInputChange); // Не использовать, так как событие происходит во время потери фокуса
+
+refs.input.addEventListener('input', onInputChange);
+refs.licenseCheckbox.addEventListener('change', onLicenseChange);
+
+function onInputFocus() {
+  console.log('Imput has focus - event focus');
+}
+
+function onInputBlur() {
+  console.log('Input lost focus - event blur');
+}
+
+function onInputChange(event) {
+  refs.nameLabel.textContent = event.currentTarget.value;
+}
+
+function onLicenseChange(event) {
+  refs.btn.disabled = !event.currentTarget.checked;
 }
