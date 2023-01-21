a6a1ac54efc8435b9127e7cee321215426b35f61
Yaroslav Klimas <yarklim@gmail.com>
2023-01-21 15:55:06 +0200

    removed react files

--- js/module_7/delegation-events.js
+++ js/module_7/delegation-events.js
@@ -262,10 +262,28 @@ const refs = {
   input: document.querySelector('#filter'),
 };
 
+refs.input.addEventListener('input', _.debounce(onFilterChange, 300));
+
 const listItemsMarkup = createListItemsMarkup(tech);
 
-refs.list.innerHTML = listItemsMarkup;
+populateList(listItemsMarkup);
 
 function createListItemsMarkup(items) {
   return items.map((item) => `<li>${item.label}</li>`).join('');
 }
+
+function onFilterChange(event) {
+  const filter = event.target.value.toLowerCase();
+
+  const filteredItems = tech.filter((t) =>
+    t.label.toLowerCase().includes(filter)
+  );
+  const listItemsMarkup = createListItemsMarkup(filteredItems);
+  populateList(listItemsMarkup);
+}
+
+function populateList(markup) {
+  refs.list.innerHTML = markup;
+}
+
+//? 'Пушистый' поиск - Fuse.js
