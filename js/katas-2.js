288ce3d194edafb64ca2202f77e575cd6e94e256
Yaroslav Klimas <yarklim@gmail.com>
2023-09-13 01:19:52 +0300

    Update katas-2.js

--- js/katas-2.js
+++ js/katas-2.js
@@ -471,3 +471,65 @@ function lowercaseCount(str) {
 // console.log(lowercaseCount("ABC123!@€£#$%^&*()_-+=}{[]|':;?/>.<,~"));
 
 //? --------------------------------------------------------
+function presses(phrase) {
+  const phraseUp = phrase.toUpperCase();
+  let count = 0;
+
+  for (let i = 0; i < phraseUp.length; i++) {
+    // if (typeof parseInt(phraseUp[i]) === 'number') {
+    //   count += 4;
+    // } else if (phraseUp[i] === ' ') {
+    //   count += 1;
+    // } else if ((phraseUp[i].charCodeAt() - 64) % 3 === 0) {
+    //   count += 3;
+    // } else {
+    //   count += (phraseUp[i].charCodeAt() - 64) % 3;
+    // }
+  }
+
+  return count;
+}
+
+console.log(presses('WHERE DO U WANT 2 MEET L8R')); // 47
+
+//   const obj = {
+//     1: 1,
+//     A: 1,
+//     B: 2,
+//     C: 3,
+//     2: 4,
+//     D: 1,
+//     E: 2,
+//     F: 3,
+//     3: 4,
+//     G: 1,
+//     H: 2,
+//     I: 3,
+//     4: 4,
+//     J: 1,
+//     K: 2,
+//     L: 3,
+//     5: 4,
+//     M: 1,
+//     N: 2,
+//     O: 3,
+//     6: 4,
+//     P: 1,
+//     Q: 2,
+//     R: 3,
+//     S: 4,
+//     7: 5,
+//     T: 1,
+//     U: 2,
+//     V: 3,
+//     8: 4,
+//     W: 1,
+//     X: 2,
+//     Y: 3,
+//     Z: 4,
+//     9: 5,
+//     '*': 1,
+//     ' ': 1,
+//     0: 2,
+//     '#': 1,
+//   };
