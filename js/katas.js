dc2cac5f25a223f1fa46a68d154137b2344aad5c
Yaroslav Klimas <yarklim@gmail.com>
2023-09-14 00:01:42 +0300

    new kata

--- js/katas.js
+++ js/katas.js
@@ -5663,3 +5663,40 @@ function declareWinner(fighter1, fighter2, firstAttacker) {
 //   )
 // ); // "Harald"
 //? ---------------------------------------------------------
+// Расширение функциональности встроенных классов
+Array.prototype.square = function () {
+  return this.map((el) => Math.pow(el, 2));
+};
+
+Array.prototype.cube = function () {
+  return this.map((el) => Math.pow(el, 3));
+};
+
+Array.prototype.average = function () {
+  return this.length === 0
+    ? NaN
+    : this.reduce((acc, el) => acc + el, 0) / this.length;
+};
+
+Array.prototype.sum = function () {
+  return this.reduce((acc, el) => acc + el, 0);
+};
+
+Array.prototype.even = function () {
+  return this.filter((el) => el % 2 === 0);
+};
+
+Array.prototype.odd = function () {
+  return this.filter((el) => el % 2 !== 0);
+};
+
+const numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers.square()); // [1, 4, 9, 16, 25]
+console.log(numbers.cube()); // [1, 8, 27, 64, 125]
+console.log(numbers.average()); // 3
+console.log(numbers.sum()); // 15
+console.log(numbers.even()); // [2, 4]
+console.log(numbers.odd()); // [1, 3, 5]
+
+//? -----------------------------------------------------
