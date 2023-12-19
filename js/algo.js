bb698bdbcd2e3af98381a40a8312e0a57679da2b
Yaroslav Klimas <yarklim@gmail.com>
2023-12-19 15:30:27 +0200

    update algo

--- js/algo.js
+++ js/algo.js
@@ -1,14 +1,48 @@
-// Сортировка пузырьком
+//? ================= Бинарный поиск ==================
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+const binarySearch = (array, item) => {
+  let start = 0;
+  let end = array.length;
+  let middle;
+  let found = false;
+  let position = -1;
+
+  while (!found && start <= end) {
+    middle = Math.floor((start + end) / 2);
+
+    if (array[middle] === item) {
+      found = true;
+      position = middle;
+      return position;
+    }
+    if (item < array[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+  }
+
+  return position;
+};
+
+console.log(binarySearch(array, 12));
+
+//? ===================================================
+
+//? ================= Сортировка пузырьком ===============
 function toBubble(arr) {
   let count = 0;
 
   while (count < arr.length) {
     for (let i = 0; i < arr.length - 1; i++) {
       if (arr[i] > arr[i + 1]) {
-        let temp = arr[i];
+        // let temp = arr[i];
 
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
+        // arr[i] = arr[i + 1];
+        // arr[i + 1] = temp;
+
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
       }
     }
     count += 1;
@@ -19,4 +53,4 @@ function toBubble(arr) {
 
 // console.log(toBubble([3, 5, 2, 1, 4]));
 // console.log(toBubble([3, 5, 2, 1, 4, 6, 7]));
-//? ----------------------------------------------
+//? ======================================================
--- js/katas-2.js
+++ js/katas-2.js
@@ -377,15 +377,66 @@ function rank(st, we, n) {
 
   return charsSum;
 }
-console.log(
-  rank(
-    'Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin',
-    [4, 2, 1, 4, 3, 1, 2],
-    4
-  )
-); // Benjamin
-console.log(rank('Lagon,Lily', [1, 5], 2)); // Lagon
-console.log(
-  rank('COLIN,AMANDBA,AMANDAB,CAROL,PauL,JOSEPH', [1, 4, 4, 5, 2, 1], 4)
-); // "PauL"
-console.log(rank('', [4, 2, 1, 4, 3, 1, 2], 6)); // "No participants"
+// console.log(
+//   rank(
+//     'Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin',
+//     [4, 2, 1, 4, 3, 1, 2],
+//     4
+//   )
+// ); // Benjamin
+// console.log(rank('Lagon,Lily', [1, 5], 2)); // Lagon
+// console.log(
+//   rank('COLIN,AMANDBA,AMANDAB,CAROL,PauL,JOSEPH', [1, 4, 4, 5, 2, 1], 4)
+// ); // "PauL"
+// console.log(rank('', [4, 2, 1, 4, 3, 1, 2], 6)); // "No participants"
+// ? -----------------------------------------------------------------------
+/*
+6 kyu Salesman's Travel
+Функция travelпримет два параметра r(список адресов всех клиентов в виде строки) и zipcode. 
+Верните строку в следующем формате:
+zipcode:street and town,street and town,.../house number,house number,...
+
+Номера улиц должны быть в том же порядке, что и улицы, которым они принадлежат.
+
+Если данный почтовый индекс не существует в списке адресов клиентов, верните"zipcode:/"
+*/
+const ad =
+  '123 Main Street St. Louisville OH 43071,432 Main Long Road St. Louisville OH 43071,786 High Street Pollocksville NY 56432,' +
+  '54 Holy Grail Street Niagara Town ZP 32908,3200 Main Rd. Bern AE 56210,1 Gordon St. Atlanta RE 13000,' +
+  '10 Pussy Cat Rd. Chicago EX 34342,10 Gordon St. Atlanta RE 13000,58 Gordon Road Atlanta RE 13000,' +
+  '22 Tokyo Av. Tedmondville SW 43098,674 Paris bd. Abbeville AA 45521,10 Surta Alley Goodtown GG 30654,' +
+  '45 Holy Grail Al. Niagara Town ZP 32908,320 Main Al. Bern AE 56210,14 Gordon Park Atlanta RE 13000,' +
+  '100 Pussy Cat Rd. Chicago EX 34342,2 Gordon St. Atlanta RE 13000,5 Gordon Road Atlanta RE 13000,' +
+  '2200 Tokyo Av. Tedmondville SW 43098,67 Paris St. Abbeville AA 45521,11 Surta Avenue Goodtown GG 30654,' +
+  '45 Holy Grail Al. Niagara Town ZP 32918,320 Main Al. Bern AE 56215,14 Gordon Park Atlanta RE 13200,' +
+  '100 Pussy Cat Rd. Chicago EX 34345,2 Gordon St. Atlanta RE 13222,5 Gordon Road Atlanta RE 13001,' +
+  '2200 Tokyo Av. Tedmondville SW 43198,67 Paris St. Abbeville AA 45522,11 Surta Avenue Goodville GG 30655,' +
+  '2222 Tokyo Av. Tedmondville SW 43198,670 Paris St. Abbeville AA 45522,114 Surta Avenue Goodville GG 30655,' +
+  '2 Holy Grail Street Niagara Town ZP 32908,3 Main Rd. Bern AE 56210,77 Gordon St. Atlanta RE 13000';
+
+const code =
+  'OH 43071,NY 56432,ZP 32908,AE 56210,RE 13000,EX 34342,SW 43098,AA 45521,GG 30654,ZP 32908,AE 56215,RE 13200,EX 34345,' +
+  'RE 13222,RE 13001,SW 43198,AA 45522,GG 30655,XX 32321,YY 45098';
+
+function travel(r, zipcode) {
+  //   const arrCodes = code.split(',');
+  //   const arrAddress = r.split(',');
+  //   return arrAddress;
+  const filterAd = r.split(',').filter((el) => el.includes(zipcode));
+  if (filterAd.length === 0) return `${zipcode}:/`;
+  const number = filterAd.map((el) => parseInt(el));
+  const address = filterAd
+    .map((el, i) =>
+      el.slice(String(number[i]).length + 1).replace(` ${zipcode}`, '')
+    )
+    .join(',');
+  return `${zipcode}:${address}/${number.join(',')}`;
+}
+// console.log(travel(ad, code));
+// console.log(travel(ad, 'AA 45522')); // "AA 45522:Paris St. Abbeville,Paris St. Abbeville/67,670"
+// console.log(travel(ad, 'EX 34342')); // "EX 34342:Pussy Cat Rd. Chicago,Pussy Cat Rd. Chicago/10,100"
+// console.log(travel(ad, 'EX 34345')); // "EX 34345:Pussy Cat Rd. Chicago/100"
+// console.log(travel(ad, 'AA 45521')); // "AA 45521:Paris bd. Abbeville,Paris St. Abbeville/674,67"
+// console.log(travel(ad, 'AE 56215')); // "AE 56215:Main Al. Bern/320"
+// console.log(travel(ad, 'OH 43071')); // ":/"
+// ? ------------------------------------------------------------------------
